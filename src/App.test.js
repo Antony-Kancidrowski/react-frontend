cda96345b97d82c6cfde03954cbc565474dffa8d
Antony Kancidrowski <antony.kancidrowski@cidrosoft.co.uk>
2021-07-28 15:48:44 +0100

    Preliminary tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,14 @@ import App from './App';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
-test('renders learn react link', () => {
+test('renders Home', () => {
   render(<BrowserRouter basename={baseUrl}><App /></BrowserRouter>);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders The Cake Emporium', () => {
+  render(<BrowserRouter basename={baseUrl}><App /></BrowserRouter>);
+  const linkElement = screen.getByText(/The Cake Emporium/i);
   expect(linkElement).toBeInTheDocument();
 });
