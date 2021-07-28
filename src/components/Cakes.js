9a6221164d0a0995889171ab9ef3775e45dc3b80
Antony Kancidrowski <antony.kancidrowski@cidrosoft.co.uk>
2021-07-28 15:44:17 +0100

    Reading cakes

--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -50,6 +50,7 @@ export class Cakes extends Component {
     super(props);
 
     this.state = {
+      cakes: [],
       showdetail: false,
       title: 'Title',
       text: 'I will not close if you click outside me. Don\'t even try to press escape key.'
@@ -72,6 +73,9 @@ export class Cakes extends Component {
         if (data.status === 200) {
           data.json().then(( { cakes }) => {
 
+            this.setState({ cakes: cakes });
+
+            console.log(this.state.cakes);
           });
         }
       })
@@ -89,99 +93,25 @@ export class Cakes extends Component {
     return (
       <>
         <CardColumns>
-          <Card onClick={() => {
-              this.setState({ showdetail: true });
-            }
-          }>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title that wraps to a new line</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card className="p-3">
-            <blockquote className="blockquote mb-0 card-body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                erat a ante.
-              </p>
-              <footer className="blockquote-footer">
-                <small className="text-muted">
-                  Someone famous in <cite title="Source Title">Source Title</cite>
-                </small>
-              </footer>
-            </blockquote>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card bg="primary" text="white" className="text-center p-3">
-            <blockquote className="blockquote mb-0 card-body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                erat a ante.
-              </p>
-              <footer className="blockquote-footer">
-                <small className="text-muted">
-                  Someone famous in <cite title="Source Title">Source Title</cite>
-                </small>
-              </footer>
-            </blockquote>
-          </Card>
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-              <Card.Text>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Img src="holder.js/100px160" />
-          </Card>
-          <Card className="text-end">
-            <blockquote className="blockquote mb-0 card-body">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                erat a ante.
-              </p>
-              <footer className="blockquote-footer">
-                <small className="text-muted">
-                  Someone famous in <cite title="Source Title">Source Title</cite>
-                </small>
-              </footer>
-            </blockquote>
-          </Card>
-          <Card>
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
-              </Card.Text>
-              <Card.Text>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+
+          {this.state.cakes.map((cake, index) => {
+
+            return (
+              <Card key={index} onClick={() => {
+                  this.setState({ showdetail: true });
+                }
+              }>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>{cake.name}</Card.Title>
+                  <Card.Text>
+                    {cake.comment}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+
         </CardColumns>
 
         <CakeDetails show={this.state.showdetail} title={this.state.title} text={this.state.text} onClose={this.handleClose} />
