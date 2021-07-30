d3ab38d82ade9a88003ca41ec179d15867af7175
Antony Kancidrowski <antony.kancidrowski@cidrosoft.co.uk>
2021-07-30 20:07:19 +0100

    Logos and images

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -5,6 +5,9 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+import Image from 'react-bootstrap/Image';
+
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -33,7 +36,14 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">The Cake Emporium</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">
+            <div>
+              <Image src={process.env.PUBLIC_URL + '/logo64.png'} alt={'Logo'} fluid />
+            </div>
+            <div>
+              The Cake Emporium
+            </div>
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
